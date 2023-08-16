bc443894b9ae3c5c1f93d089a19f403a008e4872
DARASIMI9 <batadara168@gmail.com>
2023-08-16 12:21:10 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-main.c
+++ 0x02-functions_nested_loops/5-main.c
@@ -1,25 +1,19 @@
+#include <limits.h>
 #include "main.h"
 
 /**
- * print_sign - Printys sum
- * @n: The nmber
- * Return: 1 for all
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
  */
-int print_sign(int n)
+int main(void)
 {
-	if (n > 0)
-	{
-		_putchar(43);
-		return (1);
-	}
-	else if (n < 0)
-	{
-		_putchar(45);
-		return (-1);
-	}
-	else
-	{
-		_putchar(48);
-		return (0);
-	}
+	int r;
+
+	r = print_sign(98);
+	if (r < 0)
+		r *= -1;
+	_putchar(r + '0');
+	_putchar('\n');
+	return (0);
 }
