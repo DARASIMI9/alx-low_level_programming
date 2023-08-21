d14100a6931c5ccc2356a4808d484a332697af0b
DARASIMI9 <batadara168@gmail.com>
2023-08-21 15:58:02 -0700

    updated

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,46 +1,64 @@
-include "main.h"
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
 
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
+
+ * main - program that generates random valid
+
+ * passwords for the program 101-crackme
+
  *
- * Return: the int converted from the string
+
+ * Return: Always 0 (Success)
+
  */
-int _atoi(char *s)
+
+int main(void)
+
 {
-	int i, d, n, len, f, digit;
 
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
+        int pass[100];
 
-	while (s[len] != '\0')
-		len++;
+        int i, sum, n;
 
-	while (i < len && f == 0)
-	{
-		if (s[i] == '-')
-			++d;
 
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
-		}
-		i++;
-	}
+        sum = 0;        
 
-	if (f == 0)
-		return (0);
 
-	return (n);
+        srand(time(NULL));
+
+
+        for (i = 0; i < 100; i++)
+
+        {
+
+                pass[i] = rand() % 78;
+
+                sum += (pass[i] + '0');
+
+                putchar(pass[i] + '0');
+
+                if ((2772 - sum) - '0' < 78)
+
+                {
+
+                        n = 2772 - sum - '0';
+
+                        sum += n;
+
+                        putchar(n + '0');
+
+                        break;
+
+                }
+
+        }
+
+
+        return (0);
+
 }
